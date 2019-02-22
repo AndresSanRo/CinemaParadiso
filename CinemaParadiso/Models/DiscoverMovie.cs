9c8de5968235788bf4f25167d8a759cae33933c5
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-02-22 14:16:50 +0100

    add models DiscoverMovie and DiscoverMovieRequest

---
+++ CinemaParadiso/Models/DiscoverMovie.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaParadiso.Models
+{
+    public class DiscoverMovie
+    {
+        [JsonProperty("id")]
+        public int ID { get; set; }
+        [JsonProperty("title")]
+        public String Title { get; set; }
+        [JsonProperty("poster_path")]
+        public String PosterPath { get; set; }
+
+    }
+}
