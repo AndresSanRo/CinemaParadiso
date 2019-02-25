1e43bdff5c821a7d3a83869073bed050825263f2
Andrés <andressanchezro@hotmail.com>
2019-02-25 19:49:18 +0100

    add context and interface

---
+++ CinemaParadiso/Data/CinemaContext.cs
@@ -0,0 +1,15 @@
+﻿using CinemaParadiso.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaParadiso.Data
+{
+    public class CinemaContext : DbContext, ICinemaContext
+    {
+        public CinemaContext(DbContextOptions options) : base(options) { }
+        public DbSet<Cinephile> Cinephiles { get; set; }
+    }
+}
---
+++ CinemaParadiso/Data/ICinemaContext.cs
@@ -0,0 +1,15 @@
+﻿using CinemaParadiso.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaParadiso.Data
+{
+    public interface ICinemaContext
+    {
+        DbSet<Cinephile> Cinephiles { get; set; }
+        int SaveChanges();
+    }
+}
