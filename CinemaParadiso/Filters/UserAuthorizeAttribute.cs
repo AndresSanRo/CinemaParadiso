b94fffd5efa45f7b9886010e5e51b58128252eb9
Andrés <andressanchezro@hotmail.com>
2019-02-26 21:21:29 +0100

    add autorize filter

---
+++ CinemaParadiso/Filters/UserAuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaParadiso.Filters
+{
+    public class UserAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+            if (!user.Identity.IsAuthenticated)
+            {
+                RouteValueDictionary loginPath = new RouteValueDictionary(new { controller = "Validation", action = "Login" });
+                RedirectToRouteResult action = new RedirectToRouteResult(loginPath);
+                context.Result = action;
+            }
+        }
+    }
+}
