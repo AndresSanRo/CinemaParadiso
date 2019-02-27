cf14dfbe1099c1ea1baa73dfd2aa6267497f6402
Andrés Sánchez Robleño <andressanchezro@hotmail.com>
2019-02-27 14:20:40 +0100

    add connection string to azure DDBB

--- CinemaParadiso/Startup.cs
+++ CinemaParadiso/Startup.cs
@@ -29,9 +29,8 @@ namespace CinemaParadiso
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<MovieClient>();            
-            services.AddTransient<IRepositoryCinephile, RepositoryCinephile>();
-            //services.AddDbContext<ICinemaContext, CinemaContext>(options => options.UseSqlServer(configuration.GetConnectionString("cinemaSqlServer")));
-            services.AddDbContext<ICinemaContext, CinemaContext>(options => options.UseSqlServer(configuration.GetConnectionString("tajamar")));
+            services.AddTransient<IRepositoryCinephile, RepositoryCinephile>();            
+            services.AddDbContext<ICinemaContext, CinemaContext>(options => options.UseSqlServer(configuration.GetConnectionString("azureSqlServer")));
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
